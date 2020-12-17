bdca8f0e456e751b8770cc830ff77f6a5ff8b254
alfaz-id <alfazidtech@gmail.com>
2020-12-17 23:26:27 +0700

    string

---
+++ basic/string.js
@@ -0,0 +1,7 @@
+//with (")
+var pesan1 = "Ini adalah String dengan tanda kutip ganda";
+
+// with (')
+var pesan2 = 'Ini adalah String dengan tanda kutip tunggal';
+
+const message = new String("Hello Javascript");
