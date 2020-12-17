df93374e6b58322245d07b236677c4b2a02dd3eb
alfaz-id <alfazidtech@gmail.com>
2020-12-17 23:21:59 +0700

    array

--- basic/array.js
+++ basic/array.js
@@ -1,4 +1,16 @@
+// array string
 var names = ['Crish','Jhon','Mikel'];
 for(let i=0;i<names.length;i++){
     console.log(names[i]);
 };
+
+//array number
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+for(let i=0;i<numbers.length;i++){
+    console.log(numbers[i]);
+};
+
+//get array index
+
+let names = ['Crish','Jhon','Mikel'];
+console.log(names[2]);
